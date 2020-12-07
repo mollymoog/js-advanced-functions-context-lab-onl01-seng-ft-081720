b4eb488125053ae412000a1440c639e02bfd46f6
mollymoog <mmkover@gmail.com>
2020-12-06 21:20:06 -0500

    commit tree 5684afa2e3b3a59865904ee3554e77521051dfbb

--- index.js
+++ index.js
@@ -1,5 +1,75 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(array) {
+    return {
+        firstName: array[0], 
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(arrayArray) {
+    return arrayArray.map(array => createEmployeeRecord(array))
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, time] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(time, 10),
+        date
+    })
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, time] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(time, 10),
+        date
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(dateStamp) {
+
+    let inObj = this.timeInEvents.find(function(k) {
+        return k.date === dateStamp;
+    });
+    let inHour = inObj.hour / 100;
+
+    let outObj = this.timeOutEvents.find(function(k) {
+        return k.date === dateStamp;
+    });
+    let outHour = outObj.hour / 100;
+
+    return (outHour - inHour);
+}
+
+function wagesEarnedOnDate(dateStamp) {
+    let payPerHour = this.payPerHour;
+    let hWork = hoursWorkedOnDate.call(this, dateStamp);
+
+    return (parseFloat((payPerHour * hWork).toString()));
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(empObj) {
+        return empObj.firstName == firstName;
+    }
+    )
+}
+
+let calculatePayroll = function(srcArray) {
+    return srcArray.reduce(function(total, empObj) {
+        return total + allWagesFor.call(empObj);
+    }, 0)
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
